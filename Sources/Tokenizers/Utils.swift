659df11f202dfff947c22ea215741408fd8dbaa4
AlbertKhaydarov <aghaydarov@gmail.com>
2025-04-18 19:25:12 +0300

    edit

--- Sources/Tokenizers/Utils.swift
+++ Sources/Tokenizers/Utils.swift
@@ -11,18 +11,18 @@ import Foundation
 struct Utils {
     /// Time a block in ms
     static func time<T>(label: String, _ block: () -> T) -> T {
-        let startTime = CFAbsoluteTimeGetCurrent()
-        let result = block()
-        let diff = (CFAbsoluteTimeGetCurrent() - startTime) * 1_000
-        print("[\(label)] \(diff)ms")
-        return result
-    }
+           let startTime = DispatchTime.now()
+           let result = block()
+           let diff = Double(DispatchTime.now().uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000
+           print("[\(label)] \(diff)ms")
+           return result
+       }
     
     /// Time a block in seconds and return (output, time)
     static func time<T>(_ block: () -> T) -> (T, Double) {
-        let startTime = CFAbsoluteTimeGetCurrent()
+        let startTime = DispatchTime.now()
         let result = block()
-        let diff = CFAbsoluteTimeGetCurrent() - startTime
+        let diff = Double(DispatchTime.now().uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000_000
         return (result, diff)
     }
     
